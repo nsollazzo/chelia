7c93f9f10fcc16e6df4dd3082f783f92e0760866
nsollazzo <nsollazzo@pm.me>
2020-07-03 19:08:27 +0200

    rss medium embed test 4

--- js/rss-embed/main-rss-embed.js
+++ js/rss-embed/main-rss-embed.js
@@ -1,10 +1,10 @@
 var container = document.getElementById("rss-embed");
 if (container) {
     var css = document.createElement('link');
-    css.href = "../css/embed-rss.css";
+    css.href = "./rss-embed.css";
     css.rel = "stylesheet"
     document.getElementsByTagName('head')[0].appendChild(css);
     var script = document.createElement('script');
-    script.src = "./embed-rss.js";
+    script.src = "./rss-embed.js";
     document.getElementsByTagName('body')[0].appendChild(script);
 }
